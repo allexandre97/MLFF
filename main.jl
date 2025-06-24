0f9cdbb3d8bf09e6f6792e0680e98f6ae7367b11
Alexandre Blanco Gonzalez PI Joe Greener added 05032025 <alexandrebg@lg59.lmb.internal>
2025-06-24 11:23:25 +0100

    Added functionality to produce pairs of molecules for QM training. A master dataframe is built where each row represents a conformation. The pairs are built such that every other row (conformation) 'i' is paired with another random conformation 'j' given that this second conformation 'j' belongs to the same dataset AND has the same number of atoms as the conformation 'i'. Each conformation appears strictly once in the list of pairs. If no suitable pair is found, the return state is flagged as (i, 0) instead of (i, j)

--- main.jl
+++ main.jl
@@ -29,10 +29,29 @@ include("src/nets/trainer.jl")  # Methods and variables related to Neural Nets
 
 const global DATASETS_PATH::String = parsed_args["db"]
 const global MACEOFF_PATH::String  = "/lmb/home/alexandrebg/Documents/QuarantineScripts/JG/typing/data_kovacs2023/water"
-const global HDF5_FILES = ("SPICE-2.0.1.hdf5",)#, Just use SPICE water for now
-#                           "RNA-DIVERSE-OPENFF-DEFAULT.hdf5",
-#                           "RNA-NUCLEOSIDE-OPENFF-DEFAULT.hdf5",
-#                           "RNA-TRINUCLEOTIDE-OPENFF-DEFAULT.hdf5")
+const global HDF5_FILES = ("SPICE-2.0.1.hdf5",)
+
+const global SUBSET_N_REPEATS = Dict(
+    # SPICE
+    "SPICE Solvated Amino Acids Single Points Dataset v1.1" => 100, # 1,300   -> 130,000
+    "SPICE Ion Pairs Single Points Dataset v1.2"            => 10 , # 1,426   -> 14,260
+    "SPICE Dipeptides Single Points Dataset v1.3"           => 10 , # 33,850  -> 338,500
+    "SPICE Amino Acid Ligand v1.0"                          => 2  , # 194,174 -> 388,348
+    "SPICE Solvated PubChem Set 1 v1.0"                     => 20 , # 13,934  -> 278,680
+    "SPICE Water Clusters v1.0"                             => 100, # 1,000   -> 100,000
+    # Takaba2024 Espaloma
+    "RNA Single Point Dataset v1.0"                         => 10 , # 8,560   -> 85,600
+    "RNA Nucleoside Single Point Dataset v1.0"              => 10 , # 120     -> 1,200
+    "RNA Trinucleotide Single Point Dataset v1.0"           => 10 , # 6,080   -> 60,800
+    # Kovacs2023 MACE-OFF23
+    "MACE-OFF water"                                        => 100, # 1,681   -> 168,100
+    # GEMS
+    "GEMS crambin"                                          => 100, # 5,140   -> 514,000
+    # Condensed ΔHvap
+    "vapourisation"                                         => 2  , # 2,250   -> 4,500
+    # Condensed ΔHmix
+    "mixing"                                                => 4  , # 600     -> 2,400
+)
 
 const global FEATURE_COL_NAMES = ["MOLECULE", "ATOMIC_MASS", "FORMAL_CHARGE", "AROMATICITY", "N_BONDS", "BONDS", "ANGLES", "PROPER", "IMPROPER", "MOL_ID"]
 const global FEATURE_FILES = ("features.tsv",
@@ -44,4 +63,17 @@ const global FEATURE_DATAFRAMES = [read_feat_file(file) for file in FEATURE_FILE
 
 models, optims     = build_models()
 
-train!(models, optims)
+conf_pairs = train!(models, optims)
+
+#= for pair in conf_pairs
+    a, b = pair
+
+    if b == 0
+        continue
+    end
+
+    source_a, n_a, id_a = CONF_DATAFRAME[a,:source], CONF_DATAFRAME[a,:n_atoms], CONF_DATAFRAME[a,:conf_id]
+    source_b, n_b, id_b = CONF_DATAFRAME[b,:source], CONF_DATAFRAME[b,:n_atoms], CONF_DATAFRAME[b,:conf_id]
+    
+    println("$source_a : $n_a : $id_a, $source_b : $n_b : $id_b")
+end =#
